4701c4abdac9e193dca12759f2c67d482388f920
javiyadeep <patelaleis@gmail.com>
2026-07-16 01:04:00 +0530

    .

--- main.py
+++ main.py
@@ -15,22 +15,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-print("Loading Whisper Model...")
+print("Loading Whisper Medium Model...")
 
 model = WhisperModel(
-    "base",
+    "medium",
     device="cpu",
     compute_type="int8"
 )
 
-print("✅ Model Loaded Successfully")
+print("✅ Whisper Medium Loaded Successfully")
 
 
 @app.get("/")
 def home():
     return {
         "status": "running",
-        "model": "base"
+        "model": "medium"
     }
 
 
@@ -52,17 +52,16 @@ async def transcribe(file: UploadFile = File(...)):
         temp_path = temp.name
 
     try:
-
         segments, info = model.transcribe(
             temp_path,
-            beam_size=1,
-            best_of=1,
+            beam_size=5,
+            best_of=5,
             temperature=0,
-            condition_on_previous_text=False,
-            vad_filter=False
+            vad_filter=True,
+            condition_on_previous_text=True
         )
 
-        text = "".join(segment.text for segment in segments).strip()
+        text = " ".join(segment.text.strip() for segment in segments)
 
         return {
             "success": True,
